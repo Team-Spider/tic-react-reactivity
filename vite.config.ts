86c6d257c67c1eb3cf2a53e6239cbd17f1117e69
Nikhil Singhal <70971092+NikhilSinghal1704@users.noreply.github.com>
2025-09-04 05:21:31 +0530

    Update allowedHosts in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ["https://demo.tictactoe.nik-server.in"],
+    allowedHosts: ["demo.tictactoe.nik-server.in"],
   },
   plugins: [
     react(),
